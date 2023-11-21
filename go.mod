2a25a6d95aa526bad41ba458f7bf6520bdd5bb80
snakajima <satoshi.nakajima@gmail.com>
2023-11-21 12:19:18 +0900

    openAI call

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/snakajima/slashgptGo
 
 go 1.21.4
+
+require github.com/sashabaranov/go-openai v1.17.8 // indirect
--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
   "github.com/snakajima/slashgptGo/chatConfig"
   "github.com/snakajima/slashgptGo/chatSession"
   "fmt"
+	"context"
+	"github.com/sashabaranov/go-openai"
 )
 
 func main() {
@@ -15,4 +17,25 @@ func main() {
   c := chatConfig.New()
   s := chatSession.New(c)
   fmt.Println(s.Config.OpenAIKey)
+
+	client := openai.NewClient(s.Config.OpenAIKey)
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: "Tell me about Starwars movies!",
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		return
+	}
+
+	fmt.Println(resp.Choices[0].Message.Content)
 }
