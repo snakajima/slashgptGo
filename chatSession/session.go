4ea926c9711d136a4da0c36255606108f0f60691
snakajima <satoshi.nakajima@gmail.com>
2023-11-21 15:31:51 +0900

    call_llm

--- chatSession/session.go
+++ chatSession/session.go
@@ -3,6 +3,9 @@ package chatSession
 import (
   "github.com/snakajima/slashgptGo/chatConfig"
   "github.com/snakajima/slashgptGo/manifest"
+	"context"
+	"github.com/sashabaranov/go-openai"
+  "fmt"
 )
 
 type Session struct {
@@ -15,5 +18,24 @@ func New(config chatConfig.Config, m manifest.Manifest) Session {
 }
 
 func call_llm(session Session) string {
-  return "abc"
+	resp, err := session.Config.Client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: "Hello.",
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		return ""
+	}
+
+	fmt.Println(resp.Choices[0].Message.Content)
+  return resp.Choices[0].Message.Content
 }
